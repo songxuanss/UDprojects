3ae0ce87e28e305288c565d2bb4de428980f504e
Paul Song <songxuanss@hotmail.com>
2020-01-15 22:36:49 +0700

    finished producer

---
+++ streaming_processing/OptimizingPublicTransportation/producers/common.py
@@ -0,0 +1,6 @@
+BROKER_URL = "PLAINTEXT://localhost:9092"
+REST_PROXY_URL = "http://localhost:8082"
+SCHEMA_REGISTRY_URL = "http://localhost:8081"
+KAFKA_CONNECT_URL = "http://localhost:8083"
+KSQL_URL = "http://localhost:8088"
+POSTGRESQL_URL = "jdbc:postgresql://localhost:5432/cta"
